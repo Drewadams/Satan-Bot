e084a75354720303d0310faebe31da1caaf0ed51
Drew Adams <drewadams044@gmail.com>
2019-07-01 23:40:26 -0400

    Adding another thing to racism prots

--- Satan_Bot/index.js
+++ Satan_Bot/index.js
@@ -25,7 +25,7 @@ client.on('ready', () => {
 client.on('message', message => {
   let msgCollect = message.content;
   let msg = msgCollect.toLowerCase();
-  if (msg.includes('nigger') || msg.includes('n word') || msg.includes('n1gger') || msg.includes('n i g g e r') || msg.includes('nlgger') || msg.includes('n1gg3r')) {
+  if (msg.includes('nigger') || msg.includes('n word') || msg.includes('n1gger') || msg.includes('n i g g e r') || msg.includes('nlgger') || msg.includes('n1gg3r') || msg.includes('nigg3r')) {
     message.delete();
     console.log(`just deleted this message: ${msgCollect}`);
     message.channel.send('Fuck off you racist cunt.');
@@ -39,7 +39,7 @@ client.on('message', message => {
 client.on('messageUpdate', (oldMessage, newMessage) => {
   let msgCollect = newMessage.content;
   let msg = msgCollect.toLowerCase();
-  if(msg.includes('nigger') || msg.includes('n word') || msg.includes('n1gger') || msg.includes('n i g g e r') || msg.includes('nlgger') || msg.includes('n1gg3r')){
+  if(msg.includes('nigger') || msg.includes('n word') || msg.includes('n1gger') || msg.includes('n i g g e r') || msg.includes('nlgger') || msg.includes('n1gg3r') || msg.includes('nigg3r')){
     newMessage.delete();
     console.log(`just deleted this message: ${msgCollect}`);
     newMessage.channel.send('Fuck off you racist cunt. I can\'t believe you made me code this in you ass.');
